5790ba3d27395bac8bba79a99b69efe2f79a951e
Jiri Daněk <jdanek@redhat.com>
2020-09-24 20:20:03 +0200

    move mimick into 3rdparty

--- 3rdparty/tests/mimick/include/mimick.h
+++ 3rdparty/tests/mimick/include/mimick.h
@@ -33,12 +33,12 @@ struct mmk_va_param {
 };
 
 typedef void(*mmk_fn)(void);
-typedef struct mmk_stub *mmk_stub;
+typedef struct mmk_stub *mmk_stub_t;
 
 /**
  * An invalid stub. Used for error checking if creating a stub fails.
  */
-# define MMK_STUB_INVALID ((mmk_stub)0)
+# define MMK_STUB_INVALID ((mmk_stub_t)0)
 
 /**
  * An invalid mock. Used for error checking if creating a mock fails.
@@ -59,7 +59,7 @@ typedef struct mmk_stub *mmk_stub;
  *
  * @returns The current stub object.
  */
-mmk_stub mmk_ctx(void);
+mmk_stub_t mmk_ctx(void);
 
 /**
  * Get the user context from a stub object.
@@ -73,7 +73,7 @@ mmk_stub mmk_ctx(void);
  *
  * @returns The user context or NULL
  */
-void *mmk_stub_context(mmk_stub stub);
+void *mmk_stub_context(mmk_stub_t stub);
 
 /**
  * Creates a stub object and hijacks calls to the function targeted by
@@ -95,7 +95,7 @@ void *mmk_stub_context(mmk_stub stub);
  *
  * @returns The created stub object or MMK_STUB_INVALID.
  */
-mmk_stub mmk_stub_create(const char *target, mmk_fn fn, void *ctx);
+mmk_stub_t mmk_stub_create(const char *target, mmk_fn fn, void *ctx);
 
 /**
  * Destroy a stub object and restores the original function it targets.
@@ -108,7 +108,7 @@ mmk_stub mmk_stub_create(const char *target, mmk_fn fn, void *ctx);
  *
  * @param[in] stub  The stub object to destroy.
  */
-void mmk_stub_destroy(mmk_stub stub);
+void mmk_stub_destroy(mmk_stub_t stub);
 
 /** @} */
 
