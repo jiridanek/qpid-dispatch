979481a5fe18648cab754a70b5a9f56d2b2feb58
Jiri Danek <jdanek@redhat.com>
2021-02-20 21:50:27 +0100

    DISPATCH-1962 Fix leak of qdr_core_subscribe

--- src/router_core/router_core.c
+++ src/router_core/router_core.c
@@ -150,9 +150,6 @@ void qdr_core_free(qdr_core_t *core)
     core->router_id = 0;
     core->router_area = 0;
 
-    // I have to first run qdrc_endpoint_do_cleanup_CT src/router_core/core_link_endpoint.c:241
-    //  and only then qcm_edge_router_final_CT src/router_core/modules/edge_router/module.c:59
-
     qdr_link_route_t *link_route = 0;
     while ( (link_route = DEQ_HEAD(core->link_routes))) {
         DEQ_REMOVE_HEAD(core->link_routes);
@@ -295,9 +292,20 @@ void qdr_core_free(qdr_core_t *core)
     // Drain the general work lists
     qdr_general_handler(core);
 
+    // at this point all the conn identifiers have been freed
+    qd_hash_free(core->conn_id_hash);
+
+    qdr_agent_free(core->mgmt_agent);
+
     //
     // Free the core resources
     //
+    if (core->routers_by_mask_bit)       free(core->routers_by_mask_bit);
+    if (core->control_links_by_mask_bit) free(core->control_links_by_mask_bit);
+    if (core->data_links_by_mask_bit)    free(core->data_links_by_mask_bit);
+    if (core->neighbor_free_mask)        qd_bitmask_free(core->neighbor_free_mask);
+    if (core->rnode_conns_by_mask_bit)   free(core->rnode_conns_by_mask_bit);
+
     sys_thread_free(core->thread);
     sys_cond_free(core->action_cond);
     sys_mutex_free(core->action_lock);
@@ -310,17 +318,6 @@ void qdr_core_free(qdr_core_t *core)
             free(core->forwarders[i]);
         }
     }
-
-    // at this point all the conn identifiers have been freed
-    qd_hash_free(core->conn_id_hash);
-
-    qdr_agent_free(core->mgmt_agent);
-
-    if (core->routers_by_mask_bit)       free(core->routers_by_mask_bit);
-    if (core->control_links_by_mask_bit) free(core->control_links_by_mask_bit);
-    if (core->data_links_by_mask_bit)    free(core->data_links_by_mask_bit);
-    if (core->neighbor_free_mask)        qd_bitmask_free(core->neighbor_free_mask);
-    if (core->rnode_conns_by_mask_bit)   free(core->rnode_conns_by_mask_bit);
 
     free(core);
 }
